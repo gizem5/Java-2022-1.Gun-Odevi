9ace11ede6458ef04d6a870df335c64c8266d518
Gizem Kurtuluş <98229260+gizem5@users.noreply.github.com>
2022-09-27 15:50:01 +0300

    Java 2022 1.Gun Odevi
    Engin Demirog Java 2022 kampi

---
+++ javaHelloWorld/src/javaHelloWorld/JavaHelloWorld.java
@@ -0,0 +1,11 @@
+package javaHelloWorld;
+
+public class JavaHelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Merhaba Java") ;
+		System.out.println("Merhaba Java 2") ;
+
+	}
+}
